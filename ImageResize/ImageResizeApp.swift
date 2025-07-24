1ea201cd69e660f188d704fa2b5becd224bd4e66
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-07-24 18:22:39 +0530

    Initial Commit

---
+++ ImageResize/ImageResizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageResizeApp.swift
+//  ImageResize
+//
+//  Created by Swarajmeet Singh on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageResizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
